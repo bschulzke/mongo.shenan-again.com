876c86a9a7c3798953263ef8c9fad1dfa7cbdc91
Ben Schulzke <benschulzke@gmail.com>
2022-04-10 18:10:11 -0600

    fix: really set to port 8080

--- back-end/server.js
+++ back-end/server.js
@@ -87,7 +87,7 @@ const cardSchema = new mongoose.Schema({
       }
   });
 
-  app.listen(3000, () => console.log('Server listening on port 3000!'));
+  app.listen(8080, () => console.log('Server listening on port 8080!'));
 
 
 
